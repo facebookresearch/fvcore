ced9131b817c06b76c2663a7c714d854018904e5
Yuxin Wu <yuxinwu@fb.com>
2020-01-30 16:15:50 -0800

    lint fvcore
    Summary: ./linter.sh
    
    Reviewed By: wanyenlo, bxiong1202
    
    Differential Revision: D19649449
    
    fbshipit-source-id: afdb274f05664c92486de25bdf1841386adf000e

--- fvcore/nn/__init__.py
+++ fvcore/nn/__init__.py
@@ -1,4 +1,5 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+from .flop_count import flop_count
 from .focal_loss import (
     sigmoid_focal_loss,
     sigmoid_focal_loss_jit,
@@ -8,4 +9,3 @@ from .focal_loss import (
 from .precise_bn import get_bn_modules, update_bn_stats
 from .smooth_l1_loss import smooth_l1_loss
 from .weight_init import c2_msra_fill, c2_xavier_fill
-from .flop_count import flop_count
