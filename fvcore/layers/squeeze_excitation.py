cc4a71af86e8a40d9f013e5d13663f57b39a9358
Dave Schnizlein <dschnizlein@fb.com>
2020-12-17 08:45:21 -0800

    Add SE block and unit test
    Summary: implement a SE building block builder as a layer
    
    Reviewed By: ppwwyyxx
    
    Differential Revision: D25440768
    
    fbshipit-source-id: e42c3c732a7264c912ae82c0d63fd63a33021158

---
+++ fvcore/layers/squeeze_excitation.py
@@ -0,0 +1,175 @@
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+
+from typing import Optional
+
+import torch
+import torch.nn as nn
+
+
+class SqueezeExcitation(nn.Module):
+    """
+    Generic 2d/3d extension of Squeeze-and-Excitation (SE) block described in:
+        *Hu et al., Squeeze-and-Excitation Networks, arXiv:1709.01507*
+    Squeezing spatially and exciting channel-wise
+    """
+
+    block: nn.Module
+    is_3d: bool
+
+    def __init__(
+        self,
+        num_channels: int,
+        num_channels_reduced: Optional[int] = None,
+        reduction_ratio: float = 2.0,
+        is_3d: bool = False,
+        activation: Optional[nn.Module] = None,
+    ) -> None:
+        """
+        Args:
+            num_channels (int): Number of input channels.
+            num_channels_reduced (int):
+                Number of reduced channels. If none, uses reduction_ratio to calculate.
+            reduction_ratio (float):
+                How much num_channels should be reduced if num_channels_reduced is not provided.
+            is_3d (bool): Whether we're operating on 3d data (or 2d), default 2d.
+            activation (nn.Module): Activation function used, defaults to ReLU.
+        """
+        super().__init__()
+
+        if num_channels_reduced is None:
+            num_channels_reduced = int(num_channels // reduction_ratio)
+
+        if activation is None:
+            activation = nn.ReLU()
+
+        if is_3d:
+            conv1 = nn.Conv3d(
+                num_channels, num_channels_reduced, kernel_size=1, bias=True
+            )
+            conv2 = nn.Conv3d(
+                num_channels_reduced, num_channels, kernel_size=1, bias=True
+            )
+        else:
+            conv1 = nn.Conv2d(
+                num_channels, num_channels_reduced, kernel_size=1, bias=True
+            )
+            conv2 = nn.Conv2d(
+                num_channels_reduced, num_channels, kernel_size=1, bias=True
+            )
+
+        self.is_3d = is_3d
+        self.block = nn.Sequential(
+            conv1,
+            activation,
+            conv2,
+            nn.Sigmoid(),
+        )
+
+    def forward(self, input_tensor: torch.Tensor) -> torch.Tensor:
+        """
+        Args:
+            input_tensor: X, shape = (batch_size, num_channels, H, W).
+                For 3d X, shape = (batch_size, num_channels, T, H, W).
+            output tensor
+        """
+        mean_tensor = (
+            input_tensor.mean(dim=[2, 3, 4], keepdim=True)
+            if self.is_3d
+            else input_tensor.mean(dim=[2, 3], keepdim=True)
+        )
+        output_tensor = torch.mul(input_tensor, self.block(mean_tensor))
+
+        return output_tensor
+
+
+class SpatialSqueezeExcitation(nn.Module):
+    """
+    Generic 2d/3d extension of SE block
+        squeezing channel-wise and exciting spatially described in:
+        *Roy et al., Concurrent Spatial and Channel Squeeze & Excitation
+        in Fully Convolutional Networks, MICCAI 2018*
+    """
+
+    block: nn.Module
+
+    def __init__(
+        self,
+        num_channels: int,
+        is_3d: bool = False,
+    ) -> None:
+        """
+        Args:
+            num_channels (int): Number of input channels.
+            is_3d (bool): Whether we're operating on 3d data.
+        """
+        super().__init__()
+
+        if is_3d:
+            conv = nn.Conv3d(num_channels, 1, kernel_size=1, bias=True)
+        else:
+            conv = nn.Conv2d(num_channels, 1, kernel_size=1, bias=True)
+
+        self.block = nn.Sequential(
+            conv,
+            nn.Sigmoid(),
+        )
+
+    def forward(self, input_tensor: torch.Tensor) -> torch.Tensor:
+        """
+        Args:
+            input_tensor: X, shape = (batch_size, num_channels, H, W).
+                For 3d X, shape = (batch_size, num_channels, T, H, W).
+            output tensor
+        """
+        output_tensor = torch.mul(input_tensor, self.block(input_tensor))
+
+        return output_tensor
+
+
+class ChannelSpatialSqueezeExcitation(nn.Module):
+    """
+    Generic 2d/3d extension of concurrent spatial and channel squeeze & excitation:
+         *Roy et al., Concurrent Spatial and Channel Squeeze & Excitation
+         in Fully Convolutional Networks, arXiv:1803.02579*
+    """
+
+    def __init__(
+        self,
+        num_channels: int,
+        num_channels_reduced: Optional[int] = None,
+        reduction_ratio: float = 16.0,
+        is_3d: bool = False,
+        activation: Optional[nn.Module] = None,
+    ) -> None:
+        """
+        Args:
+            num_channels (int): Number of input channels.
+            num_channels_reduced (int):
+                Number of reduced channels. If none, uses reduction_ratio to calculate.
+            reduction_ratio (float):
+                How much num_channels should be reduced if num_channels_reduced is not provided.
+            is_3d (bool): Whether we're operating on 3d data (or 2d), default 2d.
+            activation (nn.Module): Activation function used, defaults to ReLU.
+        """
+        super().__init__()
+        self.channel = SqueezeExcitation(
+            num_channels=num_channels,
+            num_channels_reduced=num_channels_reduced,
+            reduction_ratio=reduction_ratio,
+            is_3d=is_3d,
+            activation=activation,
+        )
+        self.spatial = SpatialSqueezeExcitation(num_channels=num_channels, is_3d=is_3d)
+
+    def forward(self, input_tensor: torch.Tensor) -> torch.Tensor:
+        """
+        Args:
+            input_tensor: X, shape = (batch_size, num_channels, H, W)
+                For 3d X, shape = (batch_size, num_channels, T, H, W)
+            output tensor
+        """
+        output_tensor = torch.max(
+            self.channel(input_tensor), self.spatial(input_tensor)
+        )
+
+        return output_tensor
---
+++ tests/test_layers_squeeze_excitation.py
@@ -0,0 +1,122 @@
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+import itertools
+import unittest
+from typing import Iterable
+
+import torch
+from fvcore.layers.squeeze_excitation import (
+    SqueezeExcitation,
+    SpatialSqueezeExcitation,
+    ChannelSpatialSqueezeExcitation,
+)
+
+
+class TestSqueezeExcitation(unittest.TestCase):
+    def setUp(self) -> None:
+        super().setUp()
+        torch.manual_seed(42)
+
+    def test_build_se(self) -> None:
+        """
+        Test SE model builder.
+        """
+        for layer, num_channels, is_3d in itertools.product(
+            (
+                SqueezeExcitation,
+                SpatialSqueezeExcitation,
+                ChannelSpatialSqueezeExcitation,
+            ),
+            (16, 32),
+            (True, False),
+        ):
+            model = layer(
+                num_channels=num_channels,
+                is_3d=is_3d,
+            )
+
+            # Test forwarding.
+            for input_tensor in TestSqueezeExcitation._get_inputs(
+                num_channels=num_channels, is_3d=is_3d
+            ):
+                if input_tensor.shape[1] != num_channels:
+                    with self.assertRaises(RuntimeError):
+                        output_tensor = model(input_tensor)
+                    continue
+                else:
+                    output_tensor = model(input_tensor)
+
+                input_shape = input_tensor.shape
+                output_shape = output_tensor.shape
+
+                self.assertEqual(
+                    input_shape,
+                    output_shape,
+                    "Input shape {} is different from output shape {}".format(
+                        input_shape, output_shape
+                    ),
+                )
+
+    @staticmethod
+    def _get_inputs3d(num_channels: int = 8) -> Iterable[torch.Tensor]:
+        """
+        Provide different tensors as test cases.
+
+        Yield:
+            (torch.tensor): tensor as test case input.
+        """
+        # Prepare random tensor as test cases.
+        shapes = (
+            # Forward succeeded.
+            (1, num_channels, 5, 7, 7),
+            (2, num_channels, 5, 7, 7),
+            (4, num_channels, 5, 7, 7),
+            (4, num_channels, 5, 7, 7),
+            (4, num_channels, 7, 7, 7),
+            (4, num_channels, 7, 7, 14),
+            (4, num_channels, 7, 14, 7),
+            (4, num_channels, 7, 14, 14),
+            # Forward failed.
+            (8, num_channels * 2, 3, 7, 7),
+            (8, num_channels * 4, 5, 7, 7),
+        )
+        for shape in shapes:
+            yield torch.rand(shape)
+
+    @staticmethod
+    def _get_inputs2d(num_channels: int = 8) -> Iterable[torch.Tensor]:
+        """
+        Provide different tensors as test cases.
+
+        Yield:
+            (torch.tensor): tensor as test case input.
+        """
+        # Prepare random tensor as test cases.
+        shapes = (
+            # Forward succeeded.
+            (1, num_channels, 7, 7),
+            (2, num_channels, 7, 7),
+            (4, num_channels, 7, 7),
+            (4, num_channels, 7, 14),
+            (4, num_channels, 14, 7),
+            (4, num_channels, 14, 14),
+            # Forward failed.
+            (8, num_channels * 2, 7, 7),
+            (8, num_channels * 4, 7, 7),
+        )
+        for shape in shapes:
+            yield torch.rand(shape)
+
+    @staticmethod
+    def _get_inputs(
+        num_channels: int = 8, is_3d: bool = False
+    ) -> Iterable[torch.Tensor]:
+        """
+        Provide different tensors as test cases.
+
+        Yield:
+            (torch.tensor): tensor as test case input.
+        """
+        if is_3d:
+            return TestSqueezeExcitation._get_inputs3d(num_channels=num_channels)
+        else:
+            return TestSqueezeExcitation._get_inputs2d(num_channels=num_channels)
