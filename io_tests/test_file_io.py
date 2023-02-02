3120dd1af6a60528460e61eab42c94926c6ebbe5
generatedunixname89002005307016 <generatedunixname89002005307016@fb.com>
2023-02-01 20:28:20 -0800

    upgrade pyre version in `fbcode/vision` - batch 2
    Differential Revision: D42947615
    
    fbshipit-source-id: 47b078fdf68567220e15993ab643f85771b0d340

--- io_tests/test_file_io.py
+++ io_tests/test_file_io.py
@@ -59,6 +59,8 @@ class TestNativeIO(unittest.TestCase):
     def test_exists(self) -> None:
         # pyre-ignore
         self.assertTrue(PathManager.exists(self._tmpfile))
+        # pyre-fixme[6]: For 1st argument expected `Union[PathLike[str], str]` but
+        #  got `Optional[str]`.
         fake_path = os.path.join(self._tmpdir, uuid.uuid4().hex)
         self.assertFalse(PathManager.exists(fake_path))
 
@@ -67,6 +69,8 @@ class TestNativeIO(unittest.TestCase):
         # This is a directory, not a file, so it should fail
         self.assertFalse(PathManager.isfile(self._tmpdir))  # pyre-ignore
         # This is a non-existing path, so it should fail
+        # pyre-fixme[6]: For 1st argument expected `Union[PathLike[str], str]` but
+        #  got `Optional[str]`.
         fake_path = os.path.join(self._tmpdir, uuid.uuid4().hex)
         self.assertFalse(PathManager.isfile(fake_path))
 
@@ -77,11 +81,15 @@ class TestNativeIO(unittest.TestCase):
         # pyre-ignore
         self.assertFalse(PathManager.isdir(self._tmpfile))
         # This is a non-existing path, so it should fail
+        # pyre-fixme[6]: For 1st argument expected `Union[PathLike[str], str]` but
+        #  got `Optional[str]`.
         fake_path = os.path.join(self._tmpdir, uuid.uuid4().hex)
         self.assertFalse(PathManager.isdir(fake_path))
 
     def test_ls(self) -> None:
         # Create some files in the tempdir to ls out.
+        # pyre-fixme[6]: For 1st argument expected `typing_extensions.LiteralString`
+        #  but got `Optional[str]`.
         root_dir = os.path.join(self._tmpdir, "ls")
         os.makedirs(root_dir, exist_ok=True)
         files = sorted(["foo.txt", "bar.txt", "baz.txt"])
@@ -95,6 +103,8 @@ class TestNativeIO(unittest.TestCase):
         shutil.rmtree(root_dir)
 
     def test_mkdirs(self) -> None:
+        # pyre-fixme[6]: For 1st argument expected `typing_extensions.LiteralString`
+        #  but got `Optional[str]`.
         new_dir_path = os.path.join(self._tmpdir, "new", "tmp", "dir")
         self.assertFalse(PathManager.exists(new_dir_path))
         PathManager.mkdirs(new_dir_path)
@@ -120,6 +130,8 @@ class TestNativeIO(unittest.TestCase):
         os.remove(_symlink)
 
     def test_rm(self) -> None:
+        # pyre-fixme[6]: For 1st argument expected `typing_extensions.LiteralString`
+        #  but got `Optional[str]`.
         with open(os.path.join(self._tmpdir, "test_rm.txt"), "w") as f:
             rm_file = f.name
             f.write(self._tmpfile_contents)
@@ -162,6 +174,8 @@ class TestNativeIO(unittest.TestCase):
         PathManager.mkdirs(self._tmpdir, foo="foo")  # type: ignore
         f = PathManager.open(self._tmpfile, foo="foo")  # type: ignore
         f.close()
+        # pyre-fixme[6]: For 1st argument expected `typing_extensions.LiteralString`
+        #  but got `Optional[str]`.
         with open(os.path.join(self._tmpdir, "test_rm.txt"), "w") as f:
             rm_file = f.name
             f.write(self._tmpfile_contents)
