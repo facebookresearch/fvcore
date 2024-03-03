8bc535ae6f7ba94e377dc855922cb96293afdd85
The Knights Who Say Ni <noreply+647790274085263@fb.com>
2024-03-02 17:59:56 -0800

    update external black/usort versions
    Summary:
    Match internal versions
    
    paintitblack
    
    Reviewed By: amyreese
    
    Differential Revision: D54459585
    
    fbshipit-source-id: fcaebc775396402a4acd2205b222910abd58ab21

--- linter.sh
+++ linter.sh
@@ -4,9 +4,9 @@
 # Run this script at project root by "./linter.sh" before you commit.
 
 {
-  black --version | grep -E "22.3.0" > /dev/null
+  black --version | grep -E "24.2.0" > /dev/null
 } || {
-  echo "Linter requires 'black==22.3.0' !"
+  echo "Linter requires 'black==24.2.0' !"
   exit 1
 }
 
