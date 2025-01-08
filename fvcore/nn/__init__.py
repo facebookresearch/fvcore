06b6295f8b8e31f55d1a7195f3249b896198ce56
Gonzague de Carpentier <gonzague.decarpentier@gleamer.ai>
2025-01-08 11:47:51 +0100

    remove sigmoid_focal_loss_jit

--- fvcore/nn/__init__.py
+++ fvcore/nn/__init__.py
@@ -6,7 +6,6 @@ from .flop_count import flop_count, FlopCountAnalysis
 from .focal_loss import (
     sigmoid_focal_loss,
     sigmoid_focal_loss_star,
-    sigmoid_focal_loss_star_jit,
 )
 from .giou_loss import giou_loss
 from .parameter_count import parameter_count, parameter_count_table
